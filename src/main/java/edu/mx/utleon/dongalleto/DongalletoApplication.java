c9db127cd4c5c35d38b48b67e63dd75b022bc7e3
Biron Conchas <vayron15conchas@gmail.com>
2024-11-26 17:17:09 -0600

    Proyecto en crudo

---
+++ src/main/java/edu/mx/utleon/dongalleto/DongalletoApplication.java
@@ -0,0 +1,13 @@
+package edu.mx.utleon.dongalleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongalletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongalletoApplication.class, args);
+	}
+
+}
